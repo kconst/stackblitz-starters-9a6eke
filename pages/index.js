285a4a4625227e9983c547342d2feb608ba5c4e5
Kyle Constance <kconst@gmail.com>
2023-06-23 16:52:55 +0000

    Base example

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import React, { useState, useEffect } from 'react';
-
 /**
  * Example retrieval of images from an API and placing them within a page.
  *
+ * 1. Retrieve images from an API (https://api.slingacademy.com/v1/sample-data/photos)
  * 1. Limit the images being displayed to nine in total
  * 2. Create a 3x3 grid of the images
- * 3. 
+ * 3. Add a like button to the bottom right corner of each image, on top of each image
+ * 4. Add the ability to toggle the like button to an active state for each image, separately
  */
 
 export default function Home() {
@@ -34,8 +35,8 @@ export default function Home() {
       <main>
         {images.map((photo, index) => (
           <div key={index}>
-            <img src={photo.url} className="img-fluid" alt={photo.title} />
-            
+            <img src={photo.url} alt={photo.title} />
+            <button alt="like" className={styles['like-button']} />
           </div>
         ))}
       </main>
